786789d904d426ce7c9b3d50dd73b85475965411
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-03-25 16:31:25 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import { useState } from "react"
+import { puppyList } from "./data.js"
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  // const handleClick = pup => {
+  //   setFeatPupId(pup)
+  //   console.log(pup)
+  // }
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <div className="App">
+      {puppies.map(puppy => {
+        return (
+          <p
+            key={puppy.id}
+            onClick={() => {
+              setFeatPupId(puppy.id)
+            }}
+          >
+            {puppy.name}
+          </p>
+        )
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   )
 }
 
